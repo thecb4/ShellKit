112e991fca879be8193b371b02b701b1fba915d0
TheCB4 <cavelle@thecb4.io>
2020-02-08 09:58:17 -0500

    initial commit, with swift sh workflow.swift

--- Sources/ShellKit/Command.swift
+++ Sources/ShellKit/Command.swift
@@ -5,4 +5,16 @@
 //  Created by Cavelle Benjamin on 20-Feb-08 (06).
 //
 
-import Foundation
+//typealias Arguments = [String]
+
+struct Command {
+  
+  typealias Arguments = [String]
+  
+  public let name: String
+  public let arguments: ShellAguments
+  public let environment: ShellEnvironment?
+  public let emoji: String?
+  public let echoString: String?
+  public let directory: String
+}
