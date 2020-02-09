803580006c16b04423a75e108f919dea71a7d0a5
TheCB4 <cavelle@thecb4.io>
2020-02-08 19:16:49 -0500

    updated river.sh

--- bin/river.sh
+++ bin/river.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-swift test --generate-linuxmain
-swiftformat --swiftversion 5.1 .
-swiftlint lint .
-swift test --enable-code-coverage
+swift test --generate-linuxmain && \
+swiftformat --swiftversion 5.1 . && \
+swiftlint lint . && \
+swift test --enable-code-coverage && \
 git add . && git commit -m "$1"
